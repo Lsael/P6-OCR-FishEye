45569f843ab8a0db10a80a3973bffd54c9a798f1
Lsael <l.saelens@proton.me>
2023-07-31 13:06:58 +0200

    Added navigation with arrow between images on lightbox

--- scripts/utils/accessibility.js
+++ scripts/utils/accessibility.js
@@ -1,5 +1,16 @@
-const onKeyDown = (e) => {
-    if(e.key === "Enter") {
-      e.target.onclick()
+const onKeyDown = (event) => {
+    if(event.key === "Enter") {
+      event.target.onclick()
     }
   }
+
+const navigateWithArrow = (event) => {
+  switch(event.key) {
+    case "ArrowLeft":
+      document.querySelector("#previous-image-button").onclick();
+      break;
+    case "ArrowRight":
+      document.querySelector("#next-image-button").onclick();
+      break;
+  }
+}
